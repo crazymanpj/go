efc51386d61b4ea9bf511c24116c705fd99da953
pangjian <pangjian@cmcm.com>
2019-11-05 14:14:04 +0800

    update
    update

--- dbmakepack/controllers/default.go
+++ dbmakepack/controllers/default.go
@@ -9,6 +9,8 @@ import (
 	"github.com/widuu/goini"
 	"strings"
 	"os/exec"
+	"time"
+	"encoding/json"
 )
 
 type MainController struct {
@@ -39,10 +41,18 @@ type StopController struct{
 	beego.Controller
 }
 
+type MakePackController struct{
+	beego.Controller
+}
+
+type db_packageinfo struct{
+	taskid				int
+}
+
 var JsonOutput = map[string]interface{}{
 	"errorcode" : 0,
 	"msg" : "",
-	"data" : map[string]interface{}{},
+	// "data" : map[string]interface{}{},
 	"version" : "1.0",
 }
 
@@ -50,9 +60,9 @@ func init(){
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+	// c.Data["Website"] = "beego.me"
+	// c.Data["Email"] = "astaxie@gmail.com"
+	c.TplName = "index.html"
 }
 
 func (this *ProductController) Get(){
@@ -64,7 +74,7 @@ func (this *ProductController) Get(){
 		fmt.Println(term["id"], ":", term["name"], num)
 		productlist = append(productlist, term["name"])
 	}
-	JsonOutput["data"] = productlist
+	JsonOutput["productlist"] = productlist
 	this.Data["json"] = JsonOutput     
     this.ServeJSON()
     return
@@ -90,9 +100,11 @@ func (this *TrynoController) Get(){
 func (this *PartnerController) Get(){
 	o := orm.NewOrm()
 	qs := o.QueryTable("db_partnerlist")
+	product := this.GetString("product")
+	fmt.Println(product)
 	var partnerlist []*models.DbPartnerlist
 	var itemlist []interface{}
-	num,_ := qs.Filter("status", 1).All(&partnerlist)
+	num,_ := qs.Filter("status", 1).Filter("product", product).All(&partnerlist)
 	// for i := 0; i <num; i++{
 	// 	itemlist = append(itemlist, partnerlist[i].Partner)
 	// }
@@ -101,7 +113,7 @@ func (this *PartnerController) Get(){
 		itemlist = append(itemlist, term.Partner)
 	}
 	fmt.Println(partnerlist)
-	JsonOutput["data"] = itemlist
+	JsonOutput["partnerlist"] = itemlist
 	this.Data["json"] = JsonOutput    
 	this.ServeJSON()
 	return
@@ -109,15 +121,23 @@ func (this *PartnerController) Get(){
 
 func (this *MakepackInfoController) Get(){
 	o := orm.NewOrm()
-	qs := o.QueryTable("db_packageinfo")
+	// qs := o.QueryTable("db_packageinfo")
 	var packageinfo []*models.DbPackageinfo
-	var itemlist []interface{}
-	num,_ := qs.All(&packageinfo)
+	m_packageinfo := new(models.DbPackageinfo)
+	num,_:= o.QueryTable(m_packageinfo).OrderBy("makepackagetime").All(&packageinfo)
 	fmt.Println(num)
+	var itemlist []interface{}
+	// num,_ := qs.All(&packageinfo)
+	// 也可以直接使用对象作为表名
+	// user := new(User)
+	// qs = o.QueryTable(user) // 返回 QuerySeter
+	// fmt.Println(num)
 	for _,term := range packageinfo{
-		var item []interface{}
-		item = append(item, term.Taskid, term.Makepackagetime,term.Product, term.Isnewitem, term.Itemname, term.Tryno, term.Packagetype,
-			term.Packagemodel, term.Tid1,term.Tid2,term.Tod1,term.Tod2,term.Fixuplive,term.Islokmp,term.Specialfile,term.Localname,term.User,term.Result, term.Installxml, term.Packetxml) 
+		// var item map[string]interface{}
+		item := map[string]interface{}{"taskid" :term.Taskid, "makepackagetime" :term.Makepackagetime, "product" : term.Product, "isnewitem" : term.Isnewitem,
+	"Itemname" : term.Itemname, "tryno" : term.Tryno, "packagetype":term.Packagetype, "packagemodel":term.Packagemodel, "tid1":term.Tid1, "tid2": term.Tid2,
+	"tod1":term.Tod1, "tod2":term.Tod2,"fixuplive":term.Fixuplive,"islokmp":term.Islokmp,"specialfile":term.Specialfile,"localname":term.Localname,"user":term.User,
+	"result":term.Result, "installxml":term.Installxml, "packetxml":term.Packetxml}
 		itemlist = append(itemlist, item)
 	}
 	JsonOutput["data"] = itemlist
@@ -156,7 +176,9 @@ func (this *StopController) Post(){
 	for _,term := range processlist{
 		// fmt.Println(_)
 		fmt.Println(term)
-		c := exec.Command("taskkill.exe", "/f", "/im", term)
+		var process string
+		process = term
+		c := exec.Command("taskkill.exe", "/f", "/im", process)
 		err := c.Start()
 		fmt.Println(err)
 		if err == nil{
@@ -164,6 +186,86 @@ func (this *StopController) Post(){
 		}
 	}
 	this.Data["json"] = JsonOutput  
+	this.ServeJSON()
+	return
+}
+
+func (this *MakePackController) Post(){
+	// product := this.GetString("product")
+	// isnewitem,err := this.GetInt("isnewitem")
+	// itemname := this.GetString("itemname")
+	// tryno := this.GetString("tryno")
+	// packagetype := this.GetString("packagetype")
+	// packetmodel := this.GetString("packetmodel")
+	// tid1 := this.GetString("tid1")
+	// tid2 := this.GetString("tid2")
+	// tod1 := this.GetString("tod1")
+	// tod2  := this.GetString("tod2")
+	// fixuplive,err := this.GetInt("fixuplive")
+	// islokmp,err := this.GetInt("islokmp")
+	// specialfile := this.GetString("specialfile")
+	// localname := this.GetString("localname")
+	// user := this.GetString("user")
+	// result,err := this.GetInt("result")
+	// installxml := this.GetString("installxml")
+	// packetxml := this.GetString("packetxml")
+	// mtime := time.Now().Format("2006-01-02 15:04:05")
+
+	o := orm.NewOrm()
+	var packageinfo models.DbPackageinfo
+	data := this.Ctx.Input.RequestBody
+	err := json.Unmarshal(data, &packageinfo)
+	if err != nil{
+		fmt.Println("json.Unmarshal is err:", err.Error())
+	}
+	fmt.Println(packageinfo.Product)
+	packageinfo.Makepackagetime = time.Now()
+	// packageinfo.Makepackagetime = time.Now()
+	// packageinfo.Product = product
+	// packageinfo.Isnewitem = isnewitem
+	// packageinfo.Itemname = itemname
+	// packageinfo.Tryno = tryno
+	// packageinfo.Packagetype = packagetype
+	// packageinfo.Packagemodel = packetmodel
+	// packageinfo.Tid1 = tid1
+	// packageinfo.Tid2 = tid2
+	// packageinfo.Tod1 = tod1
+	// packageinfo.Tod2 = tod2
+	// packageinfo.Fixuplive = fixuplive
+	// packageinfo.Islokmp = islokmp
+	// packageinfo.Specialfile = specialfile
+	// packageinfo.Localname = localname
+	// packageinfo.User = user
+	// packageinfo.Result = result
+	// packageinfo.Installxml = installxml
+	// packageinfo.Packetxml = packetxml
+
+	id, err := o.Insert(&packageinfo)
+	if err == nil{
+		fmt.Println(id)
+	}else{
+		fmt.Println("插入失败")
+		fmt.Println(err)
+	}
+
+	this.Data["json"] = JsonOutput  
+	JsonOutput["msg"] = "ok"
+	filename := "D:\\svn\\DubaTest\\Tools\\makedata\\makedata.exe"
+	argvs := fmt.Sprintf("-product=%s -isnewitem=%d -itemname=%s -tryno=%s -packettype=%s -packetmodel=%s -tid1=%s -tid2=%s -tod1=%s -tod2=%s -fixuplive=%d -islokmp=%d -specialfile=%s -localname=%s -taskid=%d", 
+	packageinfo.Product, packageinfo.Isnewitem, packageinfo.Itemname, packageinfo.Tryno, packageinfo.Packagetype, packageinfo.Packagemodel, packageinfo.Tid1, packageinfo.Tid2, packageinfo.Tod2, packageinfo.Tod2, 
+	packageinfo.Fixuplive, packageinfo.Islokmp, packageinfo.Specialfile, packageinfo.Localname, packageinfo.Taskid)
+	fmt.Println(filename)
+	fmt.Println(argvs)
+	c := exec.Command(filename, argvs)
+	err = c.Start()
+	fmt.Println(err)
+	if err == nil{
+		fmt.Println(err)
+		fmt.Println("成功")
+	}else{
+		fmt.Println(err)
+		fmt.Println("失败")
+	}
 	this.ServeJSON()
 	return
 }
--- dbmakepack/routers/router.go
+++ dbmakepack/routers/router.go
@@ -13,4 +13,5 @@ func init() {
 	beego.Router("/dbpackage/getallmakepacketinfo", &controllers.MakepackInfoController{})
 	beego.Router("/dbpackage/getresultbytaskid", &controllers.ResultController{})
 	beego.Router("/dbpackage/stopmakepackage", &controllers.StopController{})
+	beego.Router("/dbpackage/makepackage", &controllers.MakePackController{})
 }
